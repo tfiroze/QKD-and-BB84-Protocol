c4c9b4acf799f7106698433b2e4cd4bed85d215d
Tehsein Firoze <73216889+tfiroze@users.noreply.github.com>
2022-03-07 11:21:22 +0530

    added a variable to the function

--- bin_string_generator.py
+++ bin_string_generator.py
@@ -1,9 +1,9 @@
 import random
 
 
-def bin_str(l):
+def bin_str(n):
     bin = ""
-    for i in range(l):
+    for i in range(n):
         temp = str(random.randint(0, 1))
         bin += temp
     return bin
