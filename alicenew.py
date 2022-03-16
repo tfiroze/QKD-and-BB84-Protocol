6603e562bf6198043a261b502f9ff52b5a9e6739
Tehs <tehseinf@gmail.com>
2022-03-16 16:23:23 +0530

    new thing

---
+++ alicenew.py
@@ -0,0 +1,29 @@
+import keypolarizer
+data="hey there how are you what is the time"
+res = ''.join(format(ord(i), '08b') for i in data)
+temp=[0]*len(res)
+for i in range(0,len(res)):
+    temp[i]=int(res[i])
+send=keypolarizer.key
+send.extend(temp)
+send.extend(keypolarizer.key)
+#print(len(send))
+#print(send)
+#print(keypolarizer.newpol)
+alicenewphot=[0]*len(send)
+def newsend():
+    for i in range(0,len(send)):
+        if(send[i]==0 and keypolarizer.newpol[i]=='p'):
+            alicenewphot[i]='v'
+        elif(send[i]==0 and keypolarizer.newpol[i]=='x'):
+            alicenewphot[i]='r'
+        elif(send[i]==1 and keypolarizer.newpol[i]=='x'):
+            alicenewphot[i]='l'
+        elif(send[i]==1 and keypolarizer.newpol[i]=='p'):
+            alicenewphot[i]='h'
+        else:
+            print(send[i])
+    return(alicenewphot)
+newsend()
+#print(len(send))
+#print(len(alicenewphot))
--- keypolarizer.py
+++ keypolarizer.py
@@ -1,9 +1,12 @@
 import compare
 key=compare.key
 
-newpol=[0]*33
+newpol=[0]*16
 for i in range(0,16):
     if(key[i]==0):
         newpol[i]='p'
     if(key[i]==1):
-        newpol[]    
+        newpol[i]='x'
+for i in range(0,20):
+    newpol.extend(newpol)    
+#print(len(newpol))
